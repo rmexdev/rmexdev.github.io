d634c70cbea1c3128a80958ac878d00d9fb60f62
rmexdev <rmexdev@proton.me>
2026-06-03 23:28:49 +0530

    added astro paper theme
    formatted
    Added OG image and Favicon
    Moved RSS feed link
    Removed unneeded posts content
    Added some CTP Mocha colors
    Updated Astro Paper config
    Removed uneeded files
    prettier formatted

---
+++ apps/portfolio/astro-paper.config.ts
@@ -0,0 +1,46 @@
+import { defineAstroPaperConfig } from './src/types/config';
+
+export default defineAstroPaperConfig({
+    site: {
+        url: 'https://rmex.dev/',
+        title: 'Rmex Dev',
+        description: 'Portfolio and Blog of Rmex Dev',
+        author: 'rmexdev',
+        profile: 'https://rmex.dev/',
+        ogImage: 'default-og.png',
+        lang: 'en',
+        timezone: 'Asia/Kolkata',
+        dir: 'ltr',
+    },
+    posts: {
+        perPage: 4,
+        perIndex: 4,
+        scheduledPostMargin: 15 * 60 * 1000,
+    },
+    features: {
+        lightAndDarkMode: true,
+        dynamicOgImage: true,
+        showArchives: true,
+        showBackButton: true,
+        editPost: {
+            enabled: false
+        },
+        search: 'pagefind',
+    },
+    socials: [
+        { name: 'github', url: 'https://github.com/rmexdev' },
+        { name: 'x', url: 'https://x.com/rmexdev' },
+        // { name: 'linkedin', url: 'https://www.linkedin.com/in/harmeet01singh/' },
+        { name: 'mail', url: 'mailto:rmexdev@proton.me' },
+    ],
+    shareLinks: [
+        { name: 'whatsapp', url: 'https://wa.me/?text=' },
+        { name: 'x', url: 'https://x.com/intent/post?url=' },
+        { name: 'telegram', url: 'https://t.me/share/url?url=' },
+        {
+            name: 'pinterest',
+            url: 'https://pinterest.com/pin/create/button/?url=',
+        },
+        { name: 'mail', url: 'mailto:?subject=See%20this%20post&body=' },
+    ],
+});
---
+++ apps/portfolio/src/content/pages/about.md
@@ -0,0 +1,8 @@
+---
+title: 'About'
+description: 'Rmex Dev here. I make Websites. Software Engineer skilled in Full Stack Development with Technologies such as Typescript, Node.js, Bun, Express, MySQL.'
+---
+
+I make Websites.
+
+Software Engineer skilled in Full Stack Development with Technologies such as Typescript, Node.js, Bun, Express, MySQL.
